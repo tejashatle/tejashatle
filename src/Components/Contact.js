1a607407fee929140a695af742ce78023d20f7d0
tejashatle <tejashatle2@gmail.com>
2023-02-12 21:43:34 +0530

    sending emails

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,16 +1,55 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Toast } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import { Icon } from "@iconify/react";
+import emailjs from 'emailjs-com';
+import { useState, useRef} from "react";
 
-function Contact() {
+function Contact(e) {
+  
+  const form = useRef();
+  const [alert, setAlert] = useState({ show: false, message: '', success: false });
+
+  const toggleToast = () => setAlert({show: false, success: true});
+
+  function sendEmail(e) {
+    e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+
+    emailjs.sendForm('service_p8jiks2', 'template_u25hn7f', form.current, 'HDZOiVaGNj7Qk_Ia4', {
+      from_name: "Sender",
+      to_name: "Tejas",
+      message: "Hey, How are you",
+    })
+      .then((result) => {
+        setAlert({
+          show: true,
+          message: 'Your information has been sent!',
+          success: true,
+      });
+          console.log("Mail sent"); //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+      }, (error) => {
+
+        setAlert({
+          show: true,
+          message: 'Your information could not be sent!',
+          success: false,
+      });
+          console.log(error.text);
+      });
+    }
+
+ 
+   
   return (
     <Container
       className="contact"
       id="contact"
       style={{ marginTop: "100px", border: "2px solid #000", padding: "40px" }}
     >
+        <Toast show={alert.show} onClose={toggleToast}  bg={alert.success ? 'success' : "danger"} >
+          <Toast.Header>{alert.message} {alert.show}</Toast.Header>
+        </Toast>
       <Row>
         <Col>
           <div className="contact-content">
@@ -20,20 +59,20 @@ function Contact() {
               </h5>
             </div>
             <div className="contact-form">
-              <Form style={{ marginLeft: "100px" }}>
+              <Form style={{ marginLeft: "100px" }} ref={form} onSubmit={sendEmail}>
                 <Col xs={8}>
                   <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="text" placeholder="Your Name" />
+                    <Form.Control type="text" placeholder="Your Name" name="from_name"/>
                   </Form.Group>
                 </Col>
                 <Col xs={8}>
                   <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="email" placeholder="Your Email" />
+                    <Form.Control type="email" placeholder="Your Email" name="from_email" />
                   </Form.Group>
                 </Col>
                 <Col xs={8}>
                   <Form.Group className="mb-3" controlId="formBasicSubject">
-                    <Form.Control type="text" placeholder="Subject" />
+                    <Form.Control type="text" placeholder="Subject" name="subject"/>
                   </Form.Group>
                 </Col>
                 <Col xs={8}>
@@ -42,6 +81,7 @@ function Contact() {
                       as="textarea"
                       rows="4"
                       placeholder="Message"
+                      name="message"
                     />
                   </Form.Group>
                 </Col>
